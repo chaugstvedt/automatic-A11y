eed2ea8784fa0688eec8713a46aab69dfc23e132
Cecilie Haugstvedt <haugstvedt@gmail.com>
2025-11-23 11:33:44 +0100

    fixes problems with eslint config that made the linter run js linting on typescript files

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,23 +4,11 @@ import js from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import tseslint from "typescript-eslint";
-import typescriptParser from '@typescript-eslint/parser';
 
 export default defineConfig([
   js.configs.recommended,
   pluginReact.configs.flat.recommended,
   ...tseslint.configs.recommended,
-	{ files: ["**/*.{js,ts,tsx}"], languageOptions: { globals: globals.browser } },
-	{ files: ["**/*.{js,ts,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{ts,tsx}"], 
-    languageOptions: { 
-      parser: typescriptParser, 
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      }, 
-    } 
-  },
   { files: ["**/*.{js,ts,tsx}"], 
     plugins: {'jsx-a11y': jsxA11y,}, 
     ...jsxA11y.flatConfigs.recommended,
